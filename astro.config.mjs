227345ec89db7b42b5dc6f1e09b91b6d51da7c8b
adoestreito <adestreito@fastly.com>
2025-02-21 11:56:08 +0100

    Tutorial finished

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,10 @@ import { defineConfig } from 'astro/config';
 
 import svelte from '@astrojs/svelte';
 
+import preact from '@astrojs/preact';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte()],
+  integrations: [svelte(), preact()],
   site: "https://astro.theblackscreensociety.com"
 });
